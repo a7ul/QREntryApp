236c594084ae572f7ed6787e685b7c14d181130b
Soham Chetan <sohamchetan@Sohams-MacBook-Pro.local>
2015-05-25 23:14:25 +0530

    fixed

--- assets/qr/javascripts/main.js
+++ assets/qr/javascripts/main.js
@@ -1,5 +1,10 @@
 $(document).ready(function(){
 	$('#reader').html5_qrcode(function(data){
+      $.post( "/user/attended", {userId:data},function( data ) {
+        alert(data);
+        $('#read').html(data);
+
+      });
 			alert('data '+ data);
 			$('#read').html(data);
 		},
