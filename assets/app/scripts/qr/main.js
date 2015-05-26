ca461e289bd1bde67b3391866785bf36d2ee56f9
Soham Chetan <sohamchetan@Sohams-MacBook-Pro.local>
2015-05-26 15:28:44 +0530

    refactored

---
+++ assets/app/scripts/qr/main.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+  $('#reader').html5_qrcode(function(data){
+      $.post( '/user/attended', {userId:data},function( data ) {
+        alert(data);
+        $('#read').html(data);
+
+      });
+    },
+    function(error){
+      $('#read_error').html(error);
+    }, function(videoError){
+      $('#vid_error').html(videoError);
+
+    }
+  );
+});
